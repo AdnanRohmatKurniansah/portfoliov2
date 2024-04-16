3664dc9599870518fdddecfb9ee9b90041ec44bf
AdnanRohmatKurniansah <atnandimas@gmail.com>
2024-04-16 22:09:41 +0700

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import tailwind from "@astrojs/tailwind";
+
+export default defineConfig({
+  integrations: [mdx(), sitemap(), tailwind()]
+});
---
+++ src/config.ts
@@ -0,0 +1,6 @@
+
+export const SITE_TITLE = 'Arksitec';
+export const SITE_DESCRIPTION = 'Arksitec';
+export const GENERATE_SLUG_FROM_TITLE = true
+export const TRANSITION_API = true
+export const Favicon = "/favicon.png"
